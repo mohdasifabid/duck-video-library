6c2766feed5d1a2c40ea08845b5004796e36c752
asif <masifabid@outlook.com>
2022-05-28 13:07:04 +0530

    navbar css updated

--- src/utilities/Navbar.jsx
+++ src/utilities/Navbar.jsx
@@ -1,52 +1,27 @@
 import { Link } from "react-router-dom";
 import { useAuthProvider } from "./authProvider";
+import "./Navbar.css";
 export const Navbar = () => {
   const { state, dispatch } = useAuthProvider();
   return (
-    <div
-      className="duck-navbar-container"
-      style={{
-        borderTop: "none",
-        borderBottom: ".125rem solid black",
-        padding: ".5rem 0",
-      }}
-    >
-      <Link
-        className="duck-navbar-left-items duck-navbar-brand duck-navbar-item"
-        to="/"
-      >
+    <div className="duck-navbar-container">
+      <Link className="duck-navbar-item duck-nav-brand" to="/">
         MyBrand
       </Link>
-      <div className="duck-navbar-right-items"></div>
       {state.isLogin ? (
-        <button
-          style={{
-            width: "8rem",
-            height: ".75rem",
-            margin: ".4rem 1rem ",
-            border: "none",
-          }}
-          className="duck-primary-btn-s duck-primary-btn"
+        <Link
+          to="/landing-page"
+          className="duck-navbar-item"
           onClick={() => {
             dispatch({ type: "LOGIN_STATUS", payload: false });
             localStorage.removeItem("encodedToken");
           }}
         >
           Logout
-        </button>
+        </Link>
       ) : (
-        <Link to="/login">
-          <button
-            style={{
-              width: "8rem",
-              height: ".75rem",
-              margin: ".4rem 1rem ",
-              border: "none",
-            }}
-            className="duck-primary-btn-s duck-primary-btn"
-          >
-            Login
-          </button>
+        <Link to="/login" className="duck-navbar-item">
+          Login
         </Link>
       )}
     </div>
