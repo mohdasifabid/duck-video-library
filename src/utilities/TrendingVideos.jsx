462e8453f1fd21b1f3c83a02ba50b98c512e2d90
asif <masifabid@outlook.com>
2022-05-28 14:15:08 +0530

    navlist added to trending page

--- src/utilities/TrendingVideos.jsx
+++ src/utilities/TrendingVideos.jsx
@@ -4,6 +4,7 @@ import "./TrendingVideos.css";
 import { useVideo } from "../useVideo";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const TrendingVideos = () => {
   const { state, dispatch } = useVideo();
@@ -55,11 +56,69 @@ export const TrendingVideos = () => {
           );
         })}
       </div>
-      <div className="trending-videos-body">
-        <div className="trending-videos-card-container">
-          {updatedData.map((item) => {
-            return <VideoCard item={item} key={item._id} />;
-          })}
+      <div className="landing-page-body-container">
+        <ol className="duck-list-content-type">
+          <Link
+            to="/"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-solid fa-house-chimney"></i>
+            </div>
+            Home
+          </Link>
+          <Link
+            to="/trending"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-brands fa-gripfire"></i>
+            </div>
+            Trending
+          </Link>
+          <Link
+            to="/playlist"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-solid fa-list"></i>
+            </div>
+            Playlist
+          </Link>
+          <Link
+            to="/liked"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-solid fa-thumbs-up"></i>
+            </div>
+            Liked
+          </Link>
+          <Link
+            to="/watch-later"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-regular fa-clock"></i>
+            </div>
+            Later
+          </Link>
+          <Link
+            to="/history"
+            className="landing-page-body-links duck-list-content-type-items"
+          >
+            <div className="duck-link-avatar-polygon-type">
+              <i className="fa-solid fa-rotate-left"></i>
+            </div>
+            History
+          </Link>
+        </ol>
+        <div className="trending-videos-body">
+          <div className="trending-videos-card-container">
+            {updatedData.map((item) => {
+              return <VideoCard item={item} key={item._id} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
