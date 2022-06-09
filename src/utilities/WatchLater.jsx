a3c1f046c50dbf9bad4687904eee4194707376e7
asif <masifabid@outlook.com>
2022-06-09 14:17:46 +0530

    **watchlater data get using getCall()

--- src/utilities/WatchLater.jsx
+++ src/utilities/WatchLater.jsx
@@ -1,27 +1,14 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useVideo } from "../useVideo";
 import { Navbar } from "./Navbar";
+import { getCall } from "./reusableFunctions";
 import { VideoCard } from "./VideoCard";
 import "./WatchLater.css";
 export const WatchLater = () => {
   const { state, dispatch } = useVideo();
-  useEffect(() => {
-    const getWatchLaterVideos = async () => {
-      const token = localStorage.getItem("encodedToken");
-      const response = await axios.get("/api/user/watchlater", {
-        headers: {
-          authorization: token,
-        },
-      });
-      if (response.status === 200) {
-        dispatch({
-          type: "GET_WATCH_LATER_VIDEOS",
-          payload: response.data.watchlater,
-        });
-      }
-    };
-    getWatchLaterVideos();
+  useEffect(async () => {
+    const data = await getCall("/api/user/watchlater");
+    dispatch({ type: "GET_WATCH_LATER_VIDEOS", payload: data.watchlater });
   }, []);
   return (
     <div>
